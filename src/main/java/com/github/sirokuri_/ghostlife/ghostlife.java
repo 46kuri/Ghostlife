735b3b3557da454314a6b88be563431f7901e7d9
46kuri_ <ghost.46kuri@gmail.com>
2021-12-30 15:43:45 +0900

    Add files via upload

---
+++ src/main/java/com/github/sirokuri_/ghostlife/ghostlife.java
@@ -0,0 +1,78 @@
+package com.github.sirokuri_.ghostlife;
+
+import com.github.sirokuri_.ghostlife.command.command;
+import com.github.sirokuri_.ghostlife.listener.sellMMgui;
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.*;
+import java.util.logging.Logger;
+
+public final class ghostlife extends JavaPlugin{
+
+    public static final Logger log = Logger.getLogger("Minecraft");
+    public static Economy econ = null;
+    private static Permission perms = null;
+    private static Chat chat = null;
+
+    @Override
+    public void onEnable() {
+        if (!setupEconomy() ) {
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        getCommand("ghostlife").setExecutor(new command(this));
+        getCommand("playerskullgive").setExecutor(new command(this));
+        getCommand("adddamege").setExecutor(new command(this));
+        getCommand("sellmmgui").setExecutor(new command(this));
+        getCommand("smg").setExecutor(new command(this));
+        Bukkit.getPluginManager().registerEvents(new sellMMgui(this), this);
+        saveDefaultConfig();
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return true;
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onBlockbreak(BlockBreakEvent e) {
+        Player player = e.getPlayer();
+        Random random = new Random();
+        String world = player.getWorld().getName();
+        int num = random.nextInt(30);
+        if(world.equals("resource")){
+            if (e.getBlock().getType() == Material.OAK_LEAVES) {
+                if (num <= 2) {
+                    if ((Objects.requireNonNull(player.getInventory().getItemInMainHand().getItemMeta())).getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', "&cトマト採取剣"))) {
+                        int emptySlot = player.getInventory().firstEmpty();
+                        if (emptySlot == -1) return;
+                        Location loc = player.getLocation();
+                        player.playSound(loc,Sound.BLOCK_BELL_USE, 2, 1);
+                        getServer().dispatchCommand(getServer().getConsoleSender(), "mm i give " + player.getName() + " tomato 2");
+                    }
+                }
+            }
+        }
+    }
+}
